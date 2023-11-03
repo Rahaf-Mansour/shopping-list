6db6444dbbfba202e9025618c0cf200c395d324f
Rahaf Mansour <rahafmansour2018@gmail.com>
2023-11-03 18:34:12 +0200

    Improve the cart page code and make its style a responsive one

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,48 +1,45 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import { PRODUCTS } from "../../data/productsData";
 import { ShopContext } from "../../context/ShopContextProvider";
 import CartItem from "../../components/CartItem/CartItem";
-import { useNavigate } from "react-router-dom";
 import CheckoutForm from "../../components/CheckoutForm";
 import "./Cart.css";
 
 export default function Cart() {
   const { cartItemCounter, getTotalCartAmount } = useContext(ShopContext);
   const [showCheckoutForm, setShowCheckoutForm] = useState(false);
-
-  const toggleCheckoutForm = () => {
-    setShowCheckoutForm(!showCheckoutForm);
-  };
-  const handleCheckout = (userData) => {
-    console.log("User name and email:", userData);
-  };
-
   const navigate = useNavigate();
   const totalAmount = getTotalCartAmount();
+
+  const toggleCheckoutForm = useCallback(() => {
+    setShowCheckoutForm((prev) => !prev);
+  }, []);
+
+  const handleCheckout = useCallback((userData) => {
+    console.log("User name and email:", userData);
+  }, []);
+
+  const continueShopping = useCallback(() => navigate("/"), [navigate]);
+
+  const renderCartItems = () => {
+    return PRODUCTS.filter((product) => cartItemCounter[product.id] !== 0).map(
+      (product) => <CartItem key={product.id} data={product} />
+    );
+  };
+
   return (
     <div className="cart">
-      <div>
-        <h1>Your Cart Items: 🛒</h1>
-      </div>
-      <div>
-        {PRODUCTS.map((product) => {
-          if (cartItemCounter[product.id] !== 0) {
-            return <CartItem data={product} />;
-          }
-          return null;
-        })}
-      </div>
+      <h1 className="cart-page-header">Your Cart Items: 🛒</h1>
+      {renderCartItems()}
       {totalAmount > 0 ? (
         <div className="continue-and-checkout">
-          <p className="total-amount"> Total Amount: ${totalAmount} </p>
-          <button onClick={() => navigate("/")}> Continue Shopping </button>
-          <button
-            onClick={() => {
-              toggleCheckoutForm();
-            }}
-          >
-            Checkout
-          </button>
+          <p className="total-amount">
+            {" "}
+            Total Amount: ${totalAmount.toFixed(2)}{" "}
+          </p>
+          <button onClick={continueShopping}> Continue Shopping </button>
+          <button onClick={toggleCheckoutForm}> Checkout </button>
         </div>
       ) : (
         <h3> Your Shopping Cart is Empty! ☹️</h3>
